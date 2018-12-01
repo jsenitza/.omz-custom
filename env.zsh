b8c95b4a4e087998121c6ac6cac4d71d68bfff7e
Jacob Senitza <JCSHoosier@users.noreply.github.com>
2018-11-30 21:37:01 -0500

    add rust to path

--- env.zsh
+++ env.zsh
@@ -1,3 +1,7 @@
+// Android
 export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+// Rust
+export PATH="$HOME/.cargo/bin:$PATH"
