4d03177e56e6e5b6adb51e7efa6051911015acff
Jacob Senitza <JCSHoosier@users.noreply.github.com>
2019-03-13 22:24:29 -0400

    fix comment error and add flutter support

--- env.zsh
+++ env.zsh
@@ -1,7 +1,10 @@
-// Android
+# Android
 export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 
-// Rust
+# Flutter
+export PATH="$PATH:`pwd`/flutter/bin"
+
+# Rust
 export PATH="$HOME/.cargo/bin:$PATH"
