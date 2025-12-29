52fc4d7c2d2570e3800ed034c1a5ea52f1085f76
Jacob Senitza <8966832+jsenitza@users.noreply.github.com>
2025-12-29 08:36:34 -0500

    Update env.zsh to include emulator path

--- env.zsh
+++ env.zsh
@@ -3,9 +3,16 @@ export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 export PATH=$PATH:$ANDROID_HOME/platform-tools/fastboot
+export PATH=$PATH:$ANDROID_HOME/emulator
 
 # Flutter
 export PATH="$PATH:$HOME/flutter/bin"
+
+# Flutterfire
+export PATH="$PATH:$HOME/.pub-cache/bin"
+
+# Python
+export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 
 # Pip
 export PATH="$PATH:/Users/jacob/Library/Python/3.9/bin"
